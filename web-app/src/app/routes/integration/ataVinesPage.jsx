bb2f790d59339d7ad001d56b79f4543bd1157746
iwangjie <345127857@qq.com>
2025-01-09 18:00:47 +0800

    feat: 添加集成中心

---
+++ web-app/src/app/routes/integration/ataVinesPage.jsx
@@ -0,0 +1,14 @@
+// src/pages/DataVinesPage.jsx
+import React from 'react';
+
+const DataVinesPage = () => {
+  return (
+    <div>
+      <h1>Data Vines Integration</h1>
+      <p>Welcome to the Data Vines integration page.</p>
+      {/* Add your page content here */}
+    </div>
+  );
+};
+
+export default DataVinesPage;
